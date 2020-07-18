ad46801463f6546db18f746788d4baeaa4ddd03e
Andras Balint <anoctus@protonmail.com>
2020-07-18 13:14:02 +1200

    minesweeper challenege completed

--- minesweeper.js
+++ minesweeper.js
@@ -1,10 +1,24 @@
 document.addEventListener('DOMContentLoaded', startGame)
 
 // Define your `board` object here!
-// var board = 
+var board = {};
+var size = 4; // default size 4
+var minesTotal = 4; // default size 4
 
 function startGame () {
-  // Don't remove this function call: it makes the game work!
+  
+  generateBoard();
+  randomMines();
+
+  for (let i = 0; i < board.cells.length; i++) {
+    board['cells'][i].surroundingMines = countSurroundingMines(board['cells'][i]);
+  }
+  
+  document.addEventListener('click', checkForWin);
+  document.addEventListener('contextmenu', checkForWin);
+  
+
+  // Don't remove this function call: it makes the game work!    
   lib.initBoard()
 }
 
@@ -14,6 +28,21 @@ function startGame () {
 // 2. Are all of the mines marked?
 function checkForWin () {
 
+  let playerWin = true;
+
+  board.cells.forEach( function(cell) {
+    if (cell.isMine && !cell.isMarked) {
+      playerWin = false;
+    };
+    if (!cell.isMine && cell.hidden) {
+      playerWin = false;
+    };
+  });
+
+  if (playerWin) {
+    lib.displayMessage('You win!')
+  }
+  
   // You can use this function call to declare a winner (once you've
   // detected that they've won, that is!)
   //   lib.displayMessage('You win!')
@@ -28,5 +57,65 @@ function checkForWin () {
 // It will return cell objects in an array. You should loop through 
 // them, counting the number of times `cell.isMine` is true.
 function countSurroundingMines (cell) {
+
+  var surrounding = lib.getSurroundingCells(cell.row, cell.col)
+  
+  let count = 0;
+
+  surrounding.forEach(function (mine) {
+    if (mine.isMine) {
+      return count++
+    };
+  });
+  return count
 }
 
+function generateBoard() {
+  //creates 'cells' empty array inside 'board' object
+  board.cells = []; 
+
+  //loop within loop to set board size according to set value (default 4)
+  for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
+      //add cell object with default values to each cells
+      let cell = {
+        row: i,
+        col: j,
+        isMine: false,
+        isMarked: false,
+        hidden: true
+      }
+      //push cells into empty 'board'
+      board.cells.push(cell);
+    }
+  }
+}
+ 
+function randomMines() {
+  // checks to how many total cells we have and creates an array with range
+  let pickCells = [];
+
+  // add as many elements to array as many maximum cells we have
+  let pick = 0;
+  while (pick < (size * size)) {
+    pickCells.push(pick);
+    pick++;
+  }
+
+  //shuffle the array with Fisher-Yates Algorithm
+  for(let i = pickCells.length - 1; i > 0; i--){
+    const j = Math.floor(Math.random() * (i+1)) // changed i to (i+1) to let the element stay in its own place
+    const temp = pickCells[i]
+    pickCells[i] = pickCells[j]
+    pickCells[j] = temp
+  }
+ 
+  //slices and updates the array to leave only as many elements as many mines are set 
+  pickCells = pickCells.slice(0, minesTotal);
+
+  
+  //set isMine to true in cells that corresponds to array elements
+  for (let i = 0; i < pickCells.length; i++) {
+    board.cells[pickCells[i]].isMine = true;
+  }
+}
